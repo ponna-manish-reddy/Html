13956ca14af13df35b969ece85135022a40a6bd8
ponna-manish-reddy <manishreddypn@gmail.com>
2026-04-02 11:55:00 +0530

    week 7

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Counterapp from "./p1";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Counterapp />
+    </div>
+  );
+}
+
+export default App;
